fbc773064e09a4e6e1365b7417af82e84fa1c6d9
Ebtehal <ebtehalneseem9@gmail.com>
2025-11-12 17:16:58 +0200

    modified:   src/components/checkout/LoginOverlay.jsx

--- src/components/checkout/LoginOverlay.jsx
+++ src/components/checkout/LoginOverlay.jsx
@@ -1,6 +1,5 @@
 // src/components/LoginOverlay.jsx
-import React from "react";
-import { Button } from "@/components/ui/button"; // shadcn button path (تأكدي من المسار عندك)
+
 
 export default function LoginOverlay({ visible, onLogin }) {
   
